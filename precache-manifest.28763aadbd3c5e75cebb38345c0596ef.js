11284c98fb8e965d8a036b5ca70481e7afff9409
Suranjana <suranjana1128@gmail.com>
2021-01-02 22:49:51 +0800

    Updates

---
+++ precache-manifest.28763aadbd3c5e75cebb38345c0596ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91dad3ff55086e8821d88dea84185efa",
+    "url": "/myfolio/index.html"
+  },
+  {
+    "revision": "9e6dcf11e7b50d119f78",
+    "url": "/myfolio/static/css/2.98e655ef.chunk.css"
+  },
+  {
+    "revision": "86d739e902628bd84607",
+    "url": "/myfolio/static/css/main.02752ce2.chunk.css"
+  },
+  {
+    "revision": "9e6dcf11e7b50d119f78",
+    "url": "/myfolio/static/js/2.ecd8ae70.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/myfolio/static/js/2.ecd8ae70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d739e902628bd84607",
+    "url": "/myfolio/static/js/main.8a2bf51c.chunk.js"
+  },
+  {
+    "revision": "91a18fd935c468feed67",
+    "url": "/myfolio/static/js/runtime-main.88aa39a2.js"
+  },
+  {
+    "revision": "805d2205a433c73550f59d64c26cc824",
+    "url": "/myfolio/static/media/profile_pic.805d2205.jpg"
+  }
+]);
